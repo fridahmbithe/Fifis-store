cd11b29b29cbe45499bac23164b8d0728628c876
fridah mbithe <fridahmbithe45@gmail.com>
2023-03-07 08:20:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <article>
+          <h1>My first react project WOOO! </h1>
+          <ul>
+            <p><b>My favourite fruits</b></p>
+            <li> Mangoes</li>
+            <li>Apples</li>
+            <li>Bananas</li>
+          </ul>
+        </article>
+        <article>
+          
+        </article>
+      </div>
+     
+     
+    </div>
+ 
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,42 @@
+import "./Navbar.css"
+export default function Navbar() {
+  return (
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        Fifi's Store
+      </a>
+      <button className="menu">
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className="navigation-menu">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/services">Services</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
